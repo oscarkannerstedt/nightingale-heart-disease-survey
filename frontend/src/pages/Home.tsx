db1d365dc61540937c2c9c9f1e6323461b05bf1a
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-10-24 10:18:55 +0200

    Added - start page html

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,20 @@
 export const Home = () => {
-    return <>
-        Home
-    </>;
+  return (
+    <div className="home-container">
+      <div className="content-box">
+        <h1>
+          Welcome to Nightingale project's survey about heart diseases and
+          cardiovascular history
+        </h1>
+        <p>
+          The survey contains about 33 questions and takes about 10 mins to
+          complete.
+        </p>
+        <p>
+          Please answer as honestly as possible to get the most accurate result.
+        </p>
+        <button className="start-button">Start survey</button>
+      </div>
+    </div>
+  );
 };
