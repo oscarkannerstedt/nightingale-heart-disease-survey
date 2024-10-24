a43afb05401a0d34e8df3cdb01d57c3671515f8b
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-10-24 10:57:10 +0200

    Added - style for home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,20 +1,26 @@
+import "../style/Home.css";
+
 export const Home = () => {
+  const handleStart = () => {
+    console.log("Frågorna skall dyka upp nu");
+  };
+
   return (
-    <div className="home-container">
-      <div className="content-box">
-        <h1>
-          Welcome to Nightingale project's survey about heart diseases and
-          cardiovascular history
-        </h1>
-        <p>
-          The survey contains about 33 questions and takes about 10 mins to
-          complete.
-        </p>
-        <p>
-          Please answer as honestly as possible to get the most accurate result.
-        </p>
-        <button className="start-button">Start survey</button>
-      </div>
+    <div className="content-box">
+      <h1>
+        Welcome to Nightingale project's survey about heart diseases and
+        cardiovascular history
+      </h1>
+      <p>
+        The survey contains about 33 questions and takes about 10 mins to
+        complete.
+      </p>
+      <p>
+        Please answer as honestly as possible to get the most accurate result.
+      </p>
+      <button className="start-button" onClick={handleStart}>
+        Start survey
+      </button>
     </div>
   );
 };
