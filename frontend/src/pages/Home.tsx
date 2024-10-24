505418457103ab3fa6f7a2e8d3ba70e2d722d94d
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-10-24 12:19:16 +0200

    Added - styling

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -12,8 +12,9 @@ export const Home = () => {
     <div className="home-container">
       <div className="content-box">
         <h1>
-          Welcome to Nightingale project's survey about heart diseases and
-          cardiovascular history
+          Welcome to Nightingale project's <br /> survey about heart diseases
+          <br />
+          and cardiovascular history
         </h1>
         <p>
           The survey contains about 33 questions and takes about 10 mins to
