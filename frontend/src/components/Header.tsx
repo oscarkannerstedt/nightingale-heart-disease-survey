3b36389558b551294d6b18eb808c166f5786eb5c
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-10-24 11:24:07 +0200

    Added - header + styling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,3 +1,12 @@
+import Logo from "../assets/logo2.svg";
+import "../style/header.css";
+
 export const Header = () => {
-    return <></>
-}
+  return (
+    <header>
+      <div className="header-container">
+        <img src={Logo} alt="Logotyp" />
+      </div>
+    </header>
+  );
+};
