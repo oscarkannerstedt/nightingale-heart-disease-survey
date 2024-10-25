4939147bb2f153e2f6a0e8cf7d836c4830b1e104
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-10-25 11:55:42 +0200

    Added - change text depending on totalScore

--- frontend/src/pages/Result.tsx
+++ frontend/src/pages/Result.tsx
@@ -6,6 +6,20 @@ export const Result = () => {
   const navigate = useNavigate();
   const { totalScore, resetScore } = useScore();
 
+  const getRiskLevel = () => {
+    if (totalScore >= -88 && totalScore <= 100) {
+      return "low risk";
+    } else if (totalScore >= 101 && totalScore <= 220) {
+      return "moderate risk";
+    } else if (totalScore >= 221 && totalScore <= 350) {
+      return "high risk";
+    } else if (totalScore >= 351) {
+      return "very high risk";
+    } else {
+      return "invalid score";
+    }
+  };
+
   const handleRestart = () => {
     resetScore();
     navigate("/survey");
@@ -20,7 +34,10 @@ export const Result = () => {
     <div className="result-container">
       <div className="content-box">
         <h1>Thank you for taking the survey!</h1>
-        <p>You got {totalScore} points</p>
+        <p>
+          You got {totalScore} points and are at {getRiskLevel()} of heart
+          problems.
+        </p>
         <div className="buttons-group">
           <button className="handleRestartButton" onClick={handleRestart}>
             Do survey again
