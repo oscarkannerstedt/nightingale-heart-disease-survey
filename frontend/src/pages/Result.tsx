d88ee8e02b60b24751f11df2906d08b85d44336b
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-10-25 10:12:15 +0200

    Added - result page

--- frontend/src/pages/Result.tsx
+++ frontend/src/pages/Result.tsx
@@ -1,3 +1,32 @@
+import { useNavigate } from "react-router-dom";
+import { useScore } from "../hooks/useScore";
+
 export const Result = () => {
-    return <>Result</>
-}
+  const navigate = useNavigate();
+  const { totalScore } = useScore();
+
+  const handleRestart = () => {
+    navigate("/survey");
+  };
+
+  const handleBackToHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="result-container">
+      <div className="content-box">
+        <h1>Thank you for taking the survey!</h1>
+        <p>resultat skall komma här! {totalScore}</p>
+        <div className="buttons-group">
+          <button className="handleRestartButton" onClick={handleRestart}>
+            Do survey again
+          </button>
+          <button className="handleBackToHomeButton" onClick={handleBackToHome}>
+            Home page
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
